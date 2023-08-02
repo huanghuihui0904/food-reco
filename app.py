aac6b8ec561bcae7273e071b0a9302bfd5ce1150
Ivana Clairine Irsan <ivana.clairine@gmail.com>
2023-08-03 00:34:02 +0800

    add info

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ st.write('Hello Food!')
 
 number = st.number_input('Number to show', min_value=1, value=5, step=1)
 if st.button('Show where to eat'):
+    st.write(f"Showing {number} place to eat!")
     st.dataframe(get_recommendation(int(number)))
