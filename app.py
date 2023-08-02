4590fb1c329d7ccd92cadef49f91523366bc3352
Ivana Clairine Irsan <ivana.clairine@gmail.com>
2023-08-03 00:32:52 +0800

    cast int

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from recommender import get_recommendation
 
 st.write('Hello Food!')
 
-number = st.number_input('Number to show')
+number = st.number_input('Number to show', min_value=1, value=5, step=1)
 if st.button('Show where to eat'):
-    st.dataframe(get_recommendation(number))
+    st.dataframe(get_recommendation(int(number)))
