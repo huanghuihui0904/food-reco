10ada805fcbb87e1265cdd1eb309186404cb1363
Ivana Clairine Irsan <ivana.clairine@gmail.com>
2023-08-03 09:51:29 +0800

    fix typo

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ col1, col2, col3 = st.columns(3)
 with col1:
     number = st.number_input('Number to show', min_value=1, value=5, step=1)
 
-if st.button('Show where to eat')
+if st.button('Show where to eat'):
     st.write(f"Showing {number} places to eat in :flag-sg:!")
     st.dataframe(get_recommendation(int(number)).reset_index(drop=True))
