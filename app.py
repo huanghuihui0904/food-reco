9d74f8dd331b336a2d203432bef94dde0a7dacb9
Ivana Clairine Irsan <ivana.clairine@gmail.com>
2023-08-03 09:42:05 +0800

    column

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from recommender import get_recommendation
 
 st.write('Hello Food!')
 
-col1, col2, col3 = st.beta_columns(3)
+col1, col2, col3 = st.columns(3)
 with col1:
     number = st.number_input('Number to show', min_value=1, value=5, step=1)
 
