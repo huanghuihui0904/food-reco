18331aee94c7b5b1b3c698d589eec447b79b233a
Ivana Clairine Irsan <ivana.clairine@gmail.com>
2023-08-03 09:36:49 +0800

    button

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from recommender import get_recommendation
 
 st.write('Hello Food!')
 
-number = st.number_input('Number to show', min_value=1, value=5, step=1)
-if st.button('Show where to eat'):
+col1, col2, col3 = st.beta_columns(3)
+with col1:
+    number = st.number_input('Number to show', min_value=1, value=5, step=1)
+
+if col2.button('Show where to eat'):
     st.write(f"Showing {number} places to eat in :flag-sg:!")
     st.dataframe(get_recommendation(int(number)).reset_index(drop=True))
