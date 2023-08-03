5009e601b4907af320fb721a9866e053a15154c9
Ivana Clairine Irsan <ivana.clairine@gmail.com>
2023-08-03 09:50:59 +0800

    button

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ st.write('Hello Food!')
 col1, col2, col3 = st.columns(3)
 with col1:
     number = st.number_input('Number to show', min_value=1, value=5, step=1)
-    clicked = st.button('Show where to eat')
 
-if clicked:
+if st.button('Show where to eat')
     st.write(f"Showing {number} places to eat in :flag-sg:!")
     st.dataframe(get_recommendation(int(number)).reset_index(drop=True))
