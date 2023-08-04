efd48d413744f16cc4835db82e84bf964ff2cfeb
Ivana Clairine Irsan <ivana.clairine@gmail.com>
2023-08-04 14:32:19 +0800

    filter

--- recommender.py
+++ recommender.py
@@ -3,7 +3,8 @@ import streamlit as st
 from icecream import ic
 import pandas as pd
 
-DATABASE_ID = "bfad3d3f2560497e9e2c60d4790ad2f7"
+DATABASE_ID_1 = "bfad3d3f2560497e9e2c60d4790ad2f7" #Want to go
+DATABASE_ID_2 = "9764a07b87724cd8958002017a93df88" #Diary
 NOTION_URL = 'https://api.notion.com/v1/databases/'
 
 import requests
@@ -12,12 +13,10 @@ class NotionSync:
     def __init__(self):
         pass    
 
-    def query_databases(self,integration_token= st.secrets["notion_token"]):
-
-        database_url = NOTION_URL + DATABASE_ID + "/query"
+    def query_databases(self, database_id = DATABASE_ID_1, integration_token= st.secrets["notion_token"]):
+        database_url = NOTION_URL + database_id + "/query"
         response = requests.post(database_url, headers={"Authorization": f"Bearer {integration_token}", "Notion-Version": "2022-06-28"})
         if response.status_code != 200:
-            # raise ApiError(f'Response Status: {response.status_code}')
             print(f'### Response Status: {response.status_code}')
         else:
             return response.json()
@@ -25,7 +24,7 @@ class NotionSync:
     def get_projects_titles(self,data_json):
         return list(data_json["results"][0]["properties"].keys())
 
-    def get_projects_data(self,data_json,projects):
+    def get_to_go_data(self, data_json, projects):
         res = {}
         projects_data = {}
         for p in projects:
@@ -64,22 +63,66 @@ class NotionSync:
             temp.append([projects_data['Notes'][j][k]['text']['content'] for k in range(len(projects_data['Notes'][j]))])
         ic(temp)
         res['Notes'] = [', '.join(temp[j]) for j in range(len(temp))]
-
         res['Status'] = [projects_data['Status'][j] for j in range(len(projects_data['Status']))]
         
-        
-        
-        return pd.DataFrame(res).drop(columns='Status')
+        return pd.DataFrame(res)
     
+    def get_diary_data(self, data_json, projects):
+        res = {}
+        projects_data = {}
+        for p in projects:
+            if p == 'Name':
+                projects_data['Name'] = [data_json["results"][i]["properties"]['Name']['title']
+                                    for i in range(len(data_json["results"]))]
+                
+            if p == 'Date':
+                # ic(data_json["results"][0]["properties"]['Notes'])
+                projects_data['Date'] = [data_json["results"][i]["properties"]['Date']
+                                    for i in range(len(data_json["results"]))]
+                
+            if p == 'Area':
+                projects_data['Area'] = [data_json["results"][i]["properties"]['Area']['multi_select']
+                                    for i in range(len(data_json["results"]))]
+                
+        ic(projects_data['Date'][0])
+        res['Name'] = [projects_data['Name'][j][0]['plain_text'] for j in range(len(projects_data['Name']))]
+        res['Date'] = [projects_data['Date'][j]['date']['start'] for j in range(len(projects_data['Date']))]
+        temp = []
+        for j in range(len(projects_data['Area'])):
+            temp.append([projects_data['Area'][j][k]['name'] for k in range(len(projects_data['Area'][j]))])
+        res['Area'] = [', '.join(temp[j]) for j in range(len(temp))]
+
+        return pd.DataFrame(res)
+    
+    
+
 def get_sample(df, n=5):
     return df.sample(min(n, len(df)))
 
-def get_recommendation(n=5):
+def get_recommendation(n=5, new_place = False):
     nsync = NotionSync()
-    data = nsync.query_databases()
+    data = nsync.query_databases(database_id=DATABASE_ID_1)
     projects = nsync.get_projects_titles(data)
-    print(projects)
-    df = nsync.get_projects_data(data,projects)
+    df1 = nsync.get_to_go_data(data, projects)
+
+    if not new_place:
+        data = nsync.query_databases(database_id=DATABASE_ID_2)
+        projects = nsync.get_projects_titles(data)
+        df2 = nsync.get_diary_data(data, projects)
+        df = df1.merge(df2, on='Name', how='outer')
+    
+        ic(list(df.columns))
+        df['Area'] = df.apply(lambda x: x['Area_x'] if pd.notnull(x['Area_x']) else x['Area_y'], axis=1)
+
+        COLUMNS = list(df1.columns)
+        ic(COLUMNS)
+        COLUMNS.append('Date')
+        df = df[COLUMNS].rename(columns={'Date': 'Last Visited'})
+
+    else:
+        df = df1[df1['Status']=='Backlog']
+
     return get_sample(df, n=n)
 
 # ic(get_recommendation(30))
+# print(get_recommendation(new_place=True))
